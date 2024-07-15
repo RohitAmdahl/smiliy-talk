f479178d878aa9f58925e7b3838c6726ae2a00a1
rohitAmdahl <rohit_designer@outlook.com>
2024-07-16 01:14:56 +0200

    first commit

---
+++ src/app/components/login/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold underline">
+        Login page is under construction
+      </h1>
+    </div>
+  );
+};
+
+export default Login;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+// @import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,6 +12,13 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        background: "#282B2C",
+        dark: "#1F2937",
+      },
+      fontFamily: {
+        fonts: ["Bebas Neue", "sans-serif"],
       },
     },
   },
