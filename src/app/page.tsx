e97dfb76c9f07ee4f8dd6109d7e52e30a224ca5b
rohitAmdahl <rohit_designer@outlook.com>
2024-07-17 01:35:56 +0200

    working on it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Signup from "@/components/signup/Signup";
+import ThemeSwitch from "@/components/theme/ThemeSwitch";
 export default function Home() {
-  return (
-    <div className="">
-      <Signup />
-      <div>
-        <h1>shadcn/ui</h1>
-        <Button size="lg">Click me</Button>
-      </div>
-    </div>
-  );
+  return <div className=""></div>;
 }
