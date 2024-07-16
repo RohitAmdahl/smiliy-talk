1e5492b57baca490c0a26567e87f20cca8a74243
rohitAmdahl <rohit_designer@outlook.com>
2024-07-17 01:47:06 +0200

    removed not used imports

--- src/components/theme/ThemeSwitch.tsx
+++ src/components/theme/ThemeSwitch.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { FiSun, FiMoon } from "react-icons/fi";
-import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
-import Image from "next/image";
 
 export default function ThemeSwitch() {
   const { setTheme, resolvedTheme } = useTheme();
