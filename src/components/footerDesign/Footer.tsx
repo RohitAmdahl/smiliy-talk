75620962e464a23deb1d6fe539486a00dfc91f8a
rohitAmdahl <rohit_designer@outlook.com>
2024-07-22 01:41:08 +0200

    footer design changed

--- src/components/footerDesign/Footer.tsx
+++ src/components/footerDesign/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="tracking-wider text-lg">
+    <footer className="tracking-wider text-lg m-10">
       <div className="border-t max-w-7xl container mt-3">
         <p className="pt-8 text-lg">© 2023 Smiliy-Talk, All rights reserved.</p>
-        <p>Made with ❤️ by Rohit</p>
+        <p>project made by Rohit</p>
         <div className="">
           <a
             className="hover:underline duration-100 ease-in-out"
