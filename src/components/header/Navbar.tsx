c900a6d52ae5dc73063e01bc3943ac0f624d2dc5
rohitAmdahl <rohit_designer@outlook.com>
2024-07-30 01:36:29 +0200

    header styles design

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -7,18 +7,18 @@ import { useRouter } from "next/router";
 const Navbar = () => {
   return (
     <header>
-      <div className="w-full backdrop-blur-md ">
-        <div className=" max-w-7xl container mx-auto flex justify-between p-4 ">
+      <div className="w-full backdrop-blur-md p-4 ">
+        <div className=" max-w-7xl container mx-auto flex justify-between mt-2 px-2 py-2 ">
           <nav className="flex">
-            <Link href="/">
+            <Link className="flex justify-center items-center " href="/">
               <Image
                 src="/favicon.png"
                 alt="smiliy-talk-logo brand image logo"
                 width={100}
                 height={100}
-                className="rounded-full border-2 border-primary p-1 "
+                className="rounded-full border-2 border-primary max-w-[70px] hidden md:block"
               />
-              <span className="uppercase flex py-1 justify-center items-center tracking-wider px-2">
+              <span className="uppercase flex py-1 tracking-wider px-2">
                 smiliy-talk
               </span>
             </Link>
